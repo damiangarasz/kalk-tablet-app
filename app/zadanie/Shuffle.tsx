0ee9dd386dd0a27532abe7d969dadc923c62570b
Damian Garasz <damian.garasz@gmail.com>
2025-07-12 09:40:17 +0200

    przepisuje 2

--- app/zadanie/Shuffle.tsx
+++ app/zadanie/Shuffle.tsx
@@ -1,28 +1,32 @@
-// type typLoL = {
-//   czyChce: boolean;
-//   dzialanie: string;
-//   wynik: number;
-// };
+type typLoL = {
+  czyChce: boolean;
+  dzialanie: string;
+  wynik: number;
+};
 
-// export default function Shuffle() {
-//   //Ustawianie tabliczki mnożenia
-//   const tabliczka: typLoL[] = [];
-//   for (let n = 2; n <= 12; n++) {
-//     for (let m = 2; m <= 12; m++) {
-//       const wynik = n * m;
-//       const obj: typLoL = {} as typLoL;
-//       const rdm = Math.floor(Math.random() * 100);
+export default function Shuffle(setTabliczka: (value: typLoL[]) => void) {
+  //Ustawianie tabliczki mnożenia
+  const tabliczka: typLoL[] = [];
+  for (let n = 2; n <= 12; n++) {
+    for (let m = 2; m <= 12; m++) {
+      const wynik = n * m;
+      const obj: typLoL = {} as typLoL;
+      const rdm = Math.floor(Math.random() * 100);
 
-//       if (rdm < 50) {
-//         obj.dzialanie = `${n}x${m}`;
-//       } else {
-//         obj.dzialanie = `${m}x${n}`;
-//       }
-//       obj.wynik = wynik;
-//       obj.czyChce = true;
-//       tabliczka.push(obj);
-//     }
-//   }
+      if (rdm < 50) {
+        obj.dzialanie = `${n}x${m}`;
+      } else {
+        obj.dzialanie = `${m}x${n}`;
+      }
+      obj.wynik = wynik;
+      obj.czyChce = true;
+      tabliczka.push(obj);
+    }
+  }
 
-//   return [tabliczka, "rafiony"];
-// }
+  console.log("halo to ja");
+
+  setTabliczka(tabliczka);
+
+  //   return [tabliczka, "rafiony"];
+}
--- app/zadanie/Zadanie.tsx
+++ app/zadanie/Zadanie.tsx
@@ -1,11 +1,28 @@
-import { View } from "react-native";
+import { useEffect, useState } from "react";
+import { Text, View } from "react-native";
 import Ekran from "./EkranZzadaniem";
 import Licznik from "./Licznik";
 import Poprawna from "./Poprawna";
+import Shuffle from "./Shuffle";
 
 export default function Zadanie() {
+  type typLoL = {
+    czyChce: boolean;
+    dzialanie: string;
+    wynik: number;
+  };
+  const [tabliczka, setTabliczka] = useState<typLoL[]>([]);
+
+  useEffect(() => {
+    console.log("lol");
+    Shuffle(setTabliczka);
+  }, []);
+
+  useEffect(() => {}, [tabliczka]);
+
   return (
     <View className="bg-blue-300 w-auto h-[33%]">
+      <Text>DUPA</Text>
       <Ekran />
       <Poprawna />
       <Licznik />
