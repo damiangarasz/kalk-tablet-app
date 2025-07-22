830c0802ab51ee9336b4da60ac815f90edb7a33f
Damian Garasz <damian.garasz@gmail.com>
2025-07-22 19:08:51 +0200

    przepisuje 4 -dodaje typeconfig flagi

--- app/index.tsx
+++ app/index.tsx
@@ -7,32 +7,13 @@ import Zadanie from "./zadanie/Zadanie";
 
 export default function Index() {
   const [wynik, setWynik] = useState<number>(0);
-  // const [sucess, setSucess] = useState<number>(0);
-  // const [shuffle, setShuffle] = useState("");
 
   return (
     <SafeAreaProvider>
       <SafeAreaView>
-        <Zadanie />
+        <Zadanie wynik={wynik} />
         <Proba />
-        <Klawiatura />
-        {/* <View className="container w-auto h-[100%] bg-green-100">
-          <View className="bg-yellow-300 w-auto h-[33%]">
-            <Text className="text-7xl">{zadanie[0]}</Text>
-            <Text className="text-7xl text-green-500">{poprawna}</Text>
-            <Text>{sucess}</Text>
-          </View>
-          <View className="bg-blue-300 w-auto h-[33%]">
-            <Text
-              id="strzal"
-              className={`text-7xl `}
-              style={{ color: `rgb(${fail},0,0)` }}
-            >
-              {wynik}
-            </Text>
-          </View>
-          
-        </View> */}
+        <Klawiatura setWynik={setWynik} />
       </SafeAreaView>
     </SafeAreaProvider>
   );
